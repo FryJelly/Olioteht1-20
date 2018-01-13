a61c70d18e2de2b4b747e903511a5a45dbc472b3
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-13 16:07:53 +0200

    Teht3 done

---
+++ Olioteht3/teht3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht3
+{
+    class teht3
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, sum;
+            Console.Write("Enter three numbers: ");
+            int.TryParse(Console.ReadLine(), out a); //kokeillaan onko input numero ja jos on tulee ulos int tyypin a arvo
+            int.TryParse(Console.ReadLine(), out b); 
+            int.TryParse(Console.ReadLine(), out c);
+            sum = a + b + c; // summa = a+b+c
+            Console.Write("Sum={0} ", sum);
+            float average = sum / 3; // keskiarvo on = (a+b+c)/3
+            Console.Write("Average={0} ", average);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Olioteht4/teht4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht4
+{
+    class teht4
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
