8ddcb2bc6ab90a6ba6c9ea18a87d38a569fd44b8
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-13 19:04:28 +0200

    teht1-10 done

--- Olioteht10/teht10.cs
+++ Olioteht10/teht10.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/* Tee ohjelma, joka alustaa sovellukseen käyttöön seuraavan taulukon arvot = [1,2,33,44,55,68,77,96,100]. Käy sovelluksessa taulukko läpi ja tulosta ruutuun "HEP"-sana aina kun taulukossa oleva luku on parillinen.*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,17 @@ namespace Olioteht10
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[] { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
 
+            for (int i = 0; i < numbers.Length; i++)
+            if (numbers[i] % 2 == 0)
+            {
+                Console.WriteLine("{0} HEP ", numbers[i]);
+            }
+            else
+            {
+                    Console.WriteLine(numbers[i]);
+            };
         }
     }
 }
