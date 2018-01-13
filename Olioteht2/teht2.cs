99a35fd3862f47f71d873bb8ce95bb71c5bb2162
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-13 15:27:51 +0200

    Teht2 done

--- Olioteht2/teht2.cs
+++ Olioteht2/teht2.cs
@@ -10,6 +10,47 @@ namespace Olioteht2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Give point limit: ");
+            string line = Console.ReadLine();
+            int number = 0;
+
+            if (int.TryParse(line, out number))
+            {
+                switch (number)
+                {
+                    case 0:
+                    case 1:
+                        Console.WriteLine("School number is 0");
+                        break;
+                    case 2:
+                    case 3:
+                        Console.WriteLine("School number is 1");
+                        break;
+                    case 4:
+                    case 5:
+                        Console.WriteLine("School number is 2");
+                        break;
+                    case 6:
+                    case 7:
+                        Console.WriteLine("School number is 3");
+                        break;
+                    case 8:
+                    case 9:
+                        Console.WriteLine("School number is 4");
+                        break;
+                    case 10:
+                    case 11:
+                    case 12:
+                        Console.WriteLine("School number is 5");
+                        break;
+
+                    default:
+                        Console.WriteLine("Not a number between 0-12");
+                        break;
+
+                }
+            }
+
         }
     }
 }
