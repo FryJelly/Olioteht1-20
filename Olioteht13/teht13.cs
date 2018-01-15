ba65cd7e59034d9f7722f59a92475559b8d72273
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-15 16:12:18 +0200

    teht 13 done

--- Olioteht13/teht13.cs
+++ Olioteht13/teht13.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Mäkihypyssä käytetään viittä arvostelutuomaria. Kirjoita ohjelma, joka kysyy arvostelupisteet yhdelle hypylle ja tulostaa tyylipisteiden summan siten, että summasta on vähennetty pois pienin ja suurin tyylipiste. 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,17 @@ namespace Olioteht13
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5];
+            int number, points = 0;
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Give points: ");
+                int.TryParse(Console.ReadLine(), out number);
+                array[i] = number;
+            }
+            points = array[0] + array[1] + array[2] + array[3] + array[4] - array.Max() - array.Min();
+            Console.WriteLine("Total points are " + points);
         }
     }
 }
