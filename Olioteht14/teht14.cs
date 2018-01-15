7df5ffeda1b00b342b55a64e6e83e81680a84e9f
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-15 16:49:20 +0200

    teht 14 kesken

---
+++ Olioteht14/teht14.cs
@@ -0,0 +1,24 @@
+﻿//Kirjoita ohjelma, joka pyytää käyttäjältä opiskelijoiden arvosanat 0-5 ohjelmointi-opintojaksosta (voit itse päättää lopetusehdon). Tulosta arvosanajakauma käyttäen tähtimerkkejä seuraavasti: 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht14
+{
+    class teht14
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[8];
+            int number;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Give grades: ");
+                int.TryParse(Console.ReadLine(), out number);
+                grades[i] = number;
+            }
+        }
+    }
+}
