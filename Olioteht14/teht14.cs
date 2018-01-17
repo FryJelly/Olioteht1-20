81d66e038cbc22d6136756a4325347e3b2b02bac
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-17 20:15:01 +0200

    teht 14 done

--- Olioteht14/teht14.cs
+++ Olioteht14/teht14.cs
@@ -11,16 +11,79 @@ namespace Olioteht14
     {
         static void Main(string[] args)
         {
-            int[] grades = new int[8];
+            int zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0;
+            int[] grades = new int[16];
             int number = 0;
+
             for (int i = 0; i < grades.Length; i++)
-            { 
-                        Console.Write("Give grades: ");
-                        int.TryParse(Console.ReadLine(), out number);
-                        grades[i] = number;
+            {
+                do
+                {
+                    Console.Write("Give grades: ");
+                    int.TryParse(Console.ReadLine(), out number);
+                    grades[i] = number;
+                    switch (number)
+                    {
+                        case 0:
+                            zero++;
+                            break;
+                        case 1:
+                            one++;
+                            break;
+                        case 2:
+                            two++;
+                            break;
+                        case 3:
+                            three++;
+                            break;
+                        case 4:
+                            four++;
+                            break;
+                        case 5:
+                            five++;
+                            break;
+                    }
+                } while (number <= 6);
+                break;
             }
             Console.WriteLine("Grades: ");
-            
+            Console.Write("0: ");
+            for (int i = 0; i < zero; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            Console.Write("1: ");
+            for (int i = 0; i < one; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            Console.Write("2: ");
+            for (int i = 0; i < two; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            Console.Write("3: ");
+            for (int i = 0; i < three; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            Console.Write("4: ");
+            for (int i = 0; i < four; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            Console.Write("5: ");
+            for (int i = 0; i < five; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+
         }
     }
 }
---
+++ Olioteht15/teht15.cs
@@ -0,0 +1,29 @@
+﻿
+/* Tee ohjelma, joka tulostaa seuraavanlaisen kuvion. Kysy käyttäjältä puunkorkeus käytä juurena kaksi korkeuden yksikkö.
+
+
+    Give a number: 7 [Enter]    
+        *
+       ***
+      *****
+     *******
+    *********
+        *
+        *
+    */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht15
+{
+    class teht15
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
