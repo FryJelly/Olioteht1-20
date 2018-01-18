61c6b4486b5f3254961d4675156f3295aee65014
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-18 14:21:20 +0200

    teht 20 done

--- Olioteht18/teht18.cs
+++ Olioteht18/teht18.cs
@@ -1,17 +1,54 @@
 ﻿//Tee ohjelma, joka kysyy käyttäjältä merkkijonon (lause). Sovelluksen tulee ilmoittaa käyttäjälle oliko annettu merkkijono palidromi. 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Olioteht18
+class Program
 {
-    class teht18
+    public static bool IsPalindrome(string value) // palindrome check
     {
-        static void Main(string[] args)
+        int min = 0;
+        int max = value.Length - 1;
+        while (true)
         {
+            if (min > max)
+            {
+                return true;
+            }
+            char a = value[min];
+            char b = value[max];
 
+            while (!char.IsLetterOrDigit(a))
+            {
+                min++;
+                if (min > max)
+                {
+                    return true;
+                }
+                a = value[min];
+            }
+
+            while (!char.IsLetterOrDigit(b))
+            {
+                max--;
+                if (min > max)
+                {
+                    return true;
+                }
+                b = value[max];
+            }
+
+            if (char.ToLower(a) != char.ToLower(b))
+            {
+                return false;
+            }
+            min++;
+            max--;
         }
+    }
+
+    static void Main()
+    {
+        Console.Write("Write something to try if it's a palindrome: ");
+        string value = Console.ReadLine();
+        Console.WriteLine("{0} = {1}", value, IsPalindrome(value));   
     }
 }
--- Olioteht20/teht20.cs
+++ Olioteht20/teht20.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Olioteht20
 {
@@ -11,7 +12,27 @@ namespace Olioteht20
     {
         static void Main(string[] args)
         {
-
+            // yatzy
+            int number;
+            Random rand = new Random();
+            int[] array = new int[5];
+            Console.WriteLine("Welcome to the worst Yatzy diceroller you've ever seen!");
+            Console.Write("Press enter to throw dice: ");
+            Console.ReadLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                number = rand.Next(1, 7);
+                array[i] = number;
+                Console.WriteLine(number);
+            }
+            if (array[0] == array[1] && array[1] == array[2] && array[2] == array[3] && array[3] == array[4] && array[4] == array[5])
+            {
+                Console.WriteLine("Yatzy!!!");
+            }
+            else
+            {
+                Console.WriteLine("You lose, better luck next time.");
+            }
         }
     }
 }
