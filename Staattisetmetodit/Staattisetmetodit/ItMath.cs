ee2d5d8d4ee66568af3fb7e4542a3af9aa7a30c5
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-25 18:38:38 +0200

    staattinen alotus

---
+++ Staattisetmetodit/Staattisetmetodit/ItMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class ItMath
+    {
+        public int number;
+        public bool onkoluku;
+        bool OnkoLuku(string syote)
+        {
+            
+            return true;
+        }
+    }
+}
