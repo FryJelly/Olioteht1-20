2b594e3dae68cb5e301dc5e412b3dbf1f722bf56
Jerry Blåfield <K9105@student.jamk.fi>
2018-01-25 18:20:12 +0200

    Perintätehtäväprojektit tehty

--- Olioteht20/teht20.cs
+++ Olioteht20/teht20.cs
@@ -19,20 +19,20 @@ namespace Olioteht20
             Console.WriteLine("Welcome to the worst Yatzy diceroller you've ever seen!");
             Console.Write("Press enter to throw dice: ");
             Console.ReadLine();
-            for (int i = 0; i < array.Length; i++)
-            {
-                number = rand.Next(1, 7);
-                array[i] = number;
-                Console.WriteLine(number);
-            }
-            if (array[0] == array[1] && array[1] == array[2] && array[2] == array[3] && array[3] == array[4] && array[4] == array[5])
-            {
-                Console.WriteLine("Yatzy!!!");
-            }
-            else
-            {
-                Console.WriteLine("You lose, better luck next time.");
-            }
+                for (int i = 0; i < array.Length; i++)
+                {
+                    number = rand.Next(1, 7);
+                    array[i] = number;
+                    Console.WriteLine(number);
+                }
+                if (array[0] == array[1] && array[1] == array[2] && array[2] == array[3] && array[3] == array[4] && array[4] == array[5])
+                {
+                    Console.WriteLine("Yatzy!!!");
+                }
+                else
+                {
+                    Console.WriteLine("You lose, better luck next time.");
+                }
         }
     }
 }
