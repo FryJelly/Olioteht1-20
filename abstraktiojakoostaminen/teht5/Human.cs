ca6a83552cfdf6909e9de3157f9d6bad7a6900b9
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-05 03:54:17 +0200

    kolmas kesken

---
+++ abstraktiojakoostaminen/teht5/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht5
+{
+    class Human : Mammal
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public int Height { get; set; }
+
+        public void Move()
+        {
+            Console.WriteLine("I move");
+        }
+        public void Grow(int age)
+        {
+            Age = age;
+        }
+    }
+}
--- abstraktiojakoostaminen/teht5/Program.cs
+++ abstraktiojakoostaminen/teht5/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/* Tehtävässä tulee toteuttaa muutamia luokkia, joiden avulla testataan abstraktien metodien toimivuutta.
+Luo abstrakti luokka Nisakas, jolla on Ika-ominaisuus sekä Liiku-metodi, jonka toteutus on jätetty toteuttamatta (abstrakti metodi).
+Peri Nisakas-luokasta Ihminen-luokka, jolla on ominaisuuksina ihmiseen yleisesti liittyviä ominaisuuksia (paino, pituus, nimi). 
+Lisää Ihminen-luokkaan metodit: Liiku ja Kasva, joista ensimmäinen tulostaa ruutuun "liikun" ja jälkimmäinen lisää ihmisen ikää yhdellä vuodella. 
+Huomioi, että Liiku-metodi ja Ikä-ominaisuus on Nisakas-luokassa
+Peri Ihminen-luokasta Vauva- ja Aikuinen-luokat. Ylikirjoita Vauva-luokassa yliluokan Liiku-metodi tulostamaan "konttaa". 
+Ylikirjoita Aikuinen-luokassa myös sama metodi ja laita se tulostamaan "kävelee". Lisää aikuiselle ominaisuus auto (String). 
+Lisää vauvalle ominaisuus vaippa (String).
+Toteuta pääohjelmassa muutamia ihmisiä, aikuisia ja vauvoja. Tulostele olioiden tietoja konsolille. */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +19,7 @@ namespace teht5
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
