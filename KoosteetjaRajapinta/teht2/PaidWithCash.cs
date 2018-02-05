c8a8ee8facca8a974b625d8db7ec256bdafea8b5
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-05 17:20:38 +0200

    Alotettu rajapintatehtävät

---
+++ KoosteetjaRajapinta/teht2/PaidWithCash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht2
+{
+    class PaidWithCash : ITransaction
+    {
+        public float Register { get; set; }
+        public float Amount { get; set; }
+        string ShowTransaction()
+        {
+            Console.WriteLine("Paid with cash:")
+        }
+        float GetAmount()
+        {
+            
+        }
+        public float ShowCash()
+        {
+            
+        }
+    }
+}
