64facf145f0e18b76c3f40c0042915b6584967cf
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-08 18:39:23 +0200

    Kokoelmaluokat alotettu

---
+++ Kokoelmaluokat/teht3/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht3
+{
+    class Card
+    {
+        public string House { get; set; }
+        public int Pos { get; set; }
+        public int Number
+        {
+            get
+            {
+                return Number;
+            }
+            set
+            {
+                if (Number <= 13 && Number >= 1)
+                {
+
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Kokoelmaluokat/teht3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+
+            foreach (Card card in cards)
+            {
+                Console.WriteLine((card.Pos+1) + "Kortti on " + card.House + "#" + card.Number);
+            }
+        }
+    }
+}
