f5916b6d2ab8611e8bdb1744970734954d5bb9b1
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-12 16:25:04 +0200

    teht2 kesken

---
+++ Poikkeuksienkasittely/teht2/Names.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht2
+{
+    class Names
+    {
+
+        public void PrintNames()
+        {
+            try
+            {
+                string text = @"D:\K9105\names.txt";
+                System.Console.WriteLine(File.Exists(text) ? "Contents of names.txt:\n": "File does not exist!");
+                if (File.Exists(text) == true)
+                {
+                    var namesFile = File.ReadAllLines(text);
+                    var namesList = new List<string>(namesFile);
+                    Console.WriteLine("Löytyi " + namesList.Count() + " riviä, ja " + " nimeä.");
+                    
+                    foreach (var name in namesList)
+                    {
+                        
+                        Console.WriteLine("Nimi " + name + " esiintyy " + name.Count() + " kertaa");
+                    }
+                }
+
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+
+        }
+
+        
+    }
+}
--- Poikkeuksienkasittely/teht2/Program.cs
+++ Poikkeuksienkasittely/teht2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace teht2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Names names = new Names();
+
+                names.PrintNames();
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
