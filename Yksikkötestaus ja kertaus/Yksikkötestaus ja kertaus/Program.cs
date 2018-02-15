a08d9a933f2828893a9390e1756254d0ddb01629
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-15 17:43:20 +0200

    delegaattiprojektit

---
+++ Yksikkötestaus ja kertaus/Yksikkötestaus ja kertaus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yksikkötestaus_ja_kertaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
