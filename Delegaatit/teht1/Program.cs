444a02abdccc896b94d5b56cec9f64bcc8af4aba
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-18 20:52:37 +0200

    teht 1-2 jotenkin done

---
+++ Delegaatit/teht1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht1
+{
+    class Program
+    {
+        delegate void Input(string syote);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string syote = Console.ReadLine();
+            string choice = "";
+            while (!choice.Contains("0"))
+            {
+                Console.WriteLine("Valitse haluamasi käsittely, voit antaa useamman käsittelyn kerralla yhtenä numerosarjana esim '123': ");
+                Console.WriteLine("1=ISOIKSI kirjaimiksi ");
+                Console.WriteLine("2=pieniksi kirjaimiksi ");
+                Console.WriteLine("3=Otsikoksi ");
+                Console.WriteLine("4=Palindormiksi ");
+                Console.WriteLine("0=lopetus /n");
+                string numbers = Console.ReadLine();
+                if (numbers.Contains("1"))
+                {
+                    Input capital = InputCapital;
+                    capital(syote);
+                }
+                if (numbers.Contains("2"))
+                {
+                    Input lower = InputLower;
+                    lower(syote);
+                }
+                if (numbers.Contains("3"))
+                {
+                    Input heading = InputHeading;
+                    heading(syote);
+                }
+                if (numbers.Contains("4"))
+                {
+                    Input reverse = InputReverse;
+                    reverse(syote);
+                }
+                choice = numbers;
+            }
+        }
+        static void InputCapital(string syote)
+        {
+            Console.WriteLine(syote.ToUpper());
+        }
+        static void InputLower(string syote)
+        {
+            Console.WriteLine(syote.ToLower());
+        }
+        static void InputHeading(string syote)
+        {
+            Console.WriteLine(syote.First().ToString().ToUpper() + syote.Substring(1).ToLower());
+        }
+        static void InputReverse(string syote)
+        {
+            char[] charArray = syote.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(charArray);
+        }
+    }
+}
---
+++ Delegaatit/teht2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace teht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Friend> friends = new List<Friend>();
+            TextReader str = new StreamReader(@"F:\K9105\tutut.csv");
+            while (!(str as StreamReader).EndOfStream)
+            {
+                var line = str.ReadLine();
+                var values = line.Split(';');
+                Friend friend = new Friend(values[0], values[1]);
+                friends.Add(friend);
+            }
+            // write names in mailbook
+            Console.WriteLine("Osoitekirjassa on " + friends.Count() + " nimeä: ");
+            foreach (Friend name in friends)
+            {
+                Console.WriteLine(name.Name);
+            }
+            // Find names starting with input
+            Console.WriteLine("Enter something to find a name with(Case sensitive): ");
+            string input = Console.ReadLine();
+            List<Friend> result = friends.FindAll(x => x.Name.StartsWith(input));
+            foreach (Friend friend in result)
+            {
+                Console.WriteLine(friend.Name + " " + friend.Email);
+            }
+            str.Close();
+
+            string file = @"F:\K9105\tutut.csv";
+            Console.WriteLine("Enter new name to add: ");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Enter email of person: ");
+            string newEmail = Console.ReadLine();
+            string asd = (newName + ";" + newEmail);
+            File.AppendAllText(file, asd);
+        }
+    }
+}
