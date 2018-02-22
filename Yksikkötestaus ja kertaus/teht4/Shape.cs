5701563ced4037ce5d6c6469d6604aadf0bb2e4a
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-22 18:40:29 +0200

    fug

---
+++ Yksikkötestaus ja kertaus/teht4/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht4
+{
+    abstract class Shape
+    {
+        public string Name;
+
+        abstract public void Area();
+        abstract public void Circumference();
+
+
+    }
+}
