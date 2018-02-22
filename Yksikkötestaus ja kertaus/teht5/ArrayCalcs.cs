628c2fe0234d489b16abc0ad06d9c339408245c2
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-22 21:01:23 +0200

    teht5 done

---
+++ Yksikkötestaus ja kertaus/teht5/ArrayCalcs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht5
+{
+    class ArrayCalcs
+    {
+        public static double Sum(double[] darray)
+        {
+            return darray.Sum();
+        }
+        public static double Average(double[] darray)
+        {
+            return darray.Average();
+        }
+        public static double Max(double[] darray)
+        {
+            return darray.Max();
+        }
+        public static double Min(double[] darray)
+        {
+            return darray.Min();
+        }
+    }
+}
--- Yksikkötestaus ja kertaus/teht5/Program.cs
+++ Yksikkötestaus ja kertaus/teht5/Program.cs
@@ -10,6 +10,13 @@ namespace teht5
     {
         static void Main(string[] args)
         {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+            Console.WriteLine("Sum = " + Math.Round(ArrayCalcs.Sum(array), 2));
+            Console.WriteLine("Avg = " + Math.Round(ArrayCalcs.Average(array), 2));
+            Console.WriteLine("Min = " + Math.Round(ArrayCalcs.Min(array), 2));
+            Console.WriteLine("Max = " + Math.Round(ArrayCalcs.Max(array), 2));
+            Console.ReadKey();
         }
     }
 }
